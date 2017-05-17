ff49acc8f17860e5f758c2b0489daae77456a861
Aldredcz <devel@aldred.cz>
2017-05-17 21:53:24 +0800

    [internal] Migrated GroupsStore

--- src/core/entities/groups/store.js
+++ src/core/entities/groups/store.js
@@ -1,71 +1,23 @@
 // @flow
-import {oysterRequestFetchGroup} from './api'
-import type {TGroup} from './types'
-import {observable, action} from 'mobx'
+import {GroupsAPI} from './api'
+import {initialState} from './types'
+import createUpdatableEntityClass from 'core/store/utils/createUpdatableEntityClass'
+import createEntityStoreClass from 'core/store/utils/createEntityStoreClass'
+import type {IEntityStore} from 'core/store/utils/createEntityStoreClass'
 
 import {persistStateSingleton} from 'core/utils/mobx'
 
-interface IGroupState {
-	data: TGroup,
-	isLoading: boolean,
-}
 
-class Group implements IGroupState {
-	@observable data = {
-		uuid: '',
-		name: null,
-	}
-	@observable isLoading = false
+export class Group extends createUpdatableEntityClass({
+	entityState: initialState,
+	update: GroupsAPI.update,
+}) {}
 
-	@action setLoading (value: boolean) {
-		this.isLoading = value
-	}
+class GroupsStore extends createEntityStoreClass({
+	EntityClass: Group,
+	fetch: GroupsAPI.fetch,
+}) {}
 
-	@action setData (
-		data: $Shape<TGroup>,
-		{clearLoading = true}: {clearLoading: boolean} = {},
-	) {
-		Object.assign(this.data, data)
+const groupsStore: IEntityStore<Group> = persistStateSingleton(new GroupsStore())
 
-		if (clearLoading) {
-			this.isLoading = false
-		}
-	}
-}
-
-export class GroupsStore {
-	@observable groups: Map<string, Group> = new Map()
-
-	@action setGroup (uuid: string, groupState?: $Shape<IGroupState>): Group {
-		let group: Group
-		if (this.groups.has(uuid)) {
-			group = (this.groups.get(uuid): any)
-		} else {
-			group = new Group()
-			this.groups.set(uuid, group)
-		}
-
-		Object.assign(group, groupState)
-
-		return group
-	}
-
-	getGroup (uuid: string): Group {
-		if (this.groups.has(uuid)) {
-			return (this.groups.get(uuid): any)
-		}
-
-		const newGroup = this.setGroup(uuid)
-
-		newGroup.setLoading(true)
-		oysterRequestFetchGroup(uuid)
-			.then((data) => {
-				newGroup.setData(data)
-			})
-
-		return newGroup
-	}
-	getEntity = this.getGroup.bind(this)
-}
-
-export default persistStateSingleton(new GroupsStore())
+export default groupsStore
