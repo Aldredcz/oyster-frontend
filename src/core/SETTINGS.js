9253f0cf3ec6ca95962701d64af3b049ae05741d
Aldredcz <devel@aldred.cz>
2017-05-22 17:05:46 +0800

    [devs] Added global method for customizing API request url in runtime.

--- src/core/SETTINGS.js
+++ src/core/SETTINGS.js
@@ -2,13 +2,29 @@
 import {BE_PROXY_PREFIX} from '../../project.constants'
 
 export type TSETTINGS = {
-	+oysterApi: string,
+	oysterApi: string,
 	+env: 'development' | 'production',
+	setApiUrl?: (apiUrl: string) => void,
+	resetApiUrl?: () => void,
 }
 
+const originalOysterApi = BE_PROXY_PREFIX
+
 const SETTINGS: TSETTINGS = {
-	oysterApi: BE_PROXY_PREFIX,
+	oysterApi: localStorage.getItem('apiUrl') || originalOysterApi,
 	env: typeof ENVIRONMENT !== 'undefined' ? ENVIRONMENT : 'production',
+}
+
+window.SETTINGS = SETTINGS
+
+SETTINGS.setApiUrl = (apiUrl) => {
+	localStorage.setItem('apiUrl', apiUrl)
+	SETTINGS.oysterApi = apiUrl
+}
+
+SETTINGS.resetApiUrl = () => {
+	localStorage.removeItem('apiUrl')
+	SETTINGS.oysterApi = originalOysterApi
 }
 
 export default SETTINGS
